1d20553c59d525affaf976857be820deca6063ce
zeba jahan <zebajahan54@gmail.com>
2025-07-10 14:50:21 +0530

    initial commit

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,24 +1,3 @@
-/*#include <iostream>
-using namespace std;
-
-int main() {
-    int n;
-    long long factorial = 1;
-
-    cout << "Enter a number: ";
-    cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        factorial = factorial * i;
-    }
-
-    cout << "Factorial of " << n << " = " << factorial;
-    return 0;
-}
-*/
-
-
-
 #include <iostream>
 using namespace std;
 int main(){
